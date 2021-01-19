1ee4656d7583b33e6b494af7afc59f0a602f93e7
deadzy <cocofaudel@gmail.com>
2021-01-19 22:02:01 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -20,21 +20,10 @@ $.ajax(
 	}
 });
 
-function numStr(a, b)
+$(document).ready(function()
 {
-  a = '' + a;
-  b = b || ' ';
-  var c = '',
-      d = 0;
-  while (a.match(/^0[0-9]/)) {
-    a = a.substr(1);
-  }
-  for (var i = a.length-1; i >= 0; i--) {
-    c = (d != 0 && d % 3 == 0) ? a[i] + b + c : a[i] + c;
-    d++;
-  }
-  return c;
-}
+    $('#choicetoken').select2();
+});
 
 function gocalcule()
 {
